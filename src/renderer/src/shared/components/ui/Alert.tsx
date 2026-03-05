5d5cc0e3944c385259b0fc1d6f9714746baf83ec
iamelme <elme.delossantos@gmail.com>
2026-03-05 12:13:22 +0800

    refactor: conditional props

--- src/renderer/src/shared/components/ui/Alert.tsx
+++ src/renderer/src/shared/components/ui/Alert.tsx
@@ -1,31 +1,42 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
-import { twMerge } from 'tailwind-merge'
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 const styles = {
   variant: {
-    default: 'bg-gray-200',
-    danger: 'bg-red-200'
-  }
-}
+    default: "bg-gray-200",
+    danger: "bg-red-200",
+    warning: "bg-yellow-200",
+    success: "bg-green-200",
+  },
+  size: {
+    xs: "py-1 px-2 tx-xs",
+    default: "py-2 px-3 tx-sm",
+  },
+};
 
-type Styles = typeof styles
+type Styles = typeof styles;
 
-type AlertProps = {
-  variant: keyof Styles['variant']
-  children: ReactNode
-} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+export type AlertProps = {
+  variant?: keyof Styles["variant"];
+  size?: keyof Styles["size"];
+  children: ReactNode;
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
 export default function Alert({
-  variant = 'default',
+  variant = "default",
+  size = "default",
   children,
   ...props
 }: AlertProps): React.ReactElement {
   return (
     <div
       {...props}
-      className={twMerge(`py-2 px-3 rounded-md ${styles.variant[variant]}`, props.className)}
+      className={twMerge(
+        `rounded-md ${styles.variant[variant]} ${styles.size[size]}`,
+        props.className,
+      )}
     >
       {children}
     </div>
-  )
+  );
 }
