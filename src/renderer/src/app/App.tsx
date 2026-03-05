fd09a613fc289b9f565deb28d67926a22cfa4bd8
iamelme <elme.delossantos@gmail.com>
2026-03-05 18:48:19 +0800

    style: code format and clean up

--- src/renderer/src/app/App.tsx
+++ src/renderer/src/app/App.tsx
@@ -1,55 +1,43 @@
-import { Outlet } from 'react-router-dom'
-import { ErrorBoundary } from 'react-error-boundary'
-import Sidebar from './layout/Sidebar'
-// import { useEffect } from 'react'
-import useBoundStore from '../shared/stores/boundStore'
-import { useQuery } from '@tanstack/react-query'
+import { Outlet } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import Sidebar from "./layout/Sidebar";
+import useBoundStore from "../shared/stores/boundStore";
+import { useQuery } from "@tanstack/react-query";
 
 function App(): React.JSX.Element {
-  const updateLocale = useBoundStore((state) => state.updateLocale)
-  const updateLogo = useBoundStore((state) => state.updateLogo)
+  const updateLocale = useBoundStore((state) => state.updateLocale);
+  const updateLogo = useBoundStore((state) => state.updateLogo);
 
   useQuery({
-    queryKey: ['settings'],
+    queryKey: ["settings"],
     queryFn: async () => {
-      const res = await window.apiSettings.getSettings()
+      const res = await window.apiSettings.getSettings();
 
-      console.log('logo', res)
       if (res.error && res.error instanceof Error) {
-        throw new Error(res.error.message)
+        throw new Error(res.error.message);
       }
 
-      updateLocale(res.data.locale)
-      updateLogo(res.data.logo)
+      updateLocale(res.data.locale);
+      updateLogo(res.data.logo);
 
-      return res.data
-    }
-  })
+      return res.data;
+    },
+  });
 
-  // useEffect(() => {
-  //   const ipcHandle = async (): Promise<void> => {
-  //     const locale = await window.apiElectron.getLocale()
-
-  //     updateLocale(locale)
-  //   }
-
-  //   ipcHandle()
-  // }, [updateLocale])
-
-  const store = useBoundStore((state) => state)
-  console.log('store ', store)
-  const updateUser = useBoundStore((state) => state.updateUser)
+  const updateUser = useBoundStore((state) => state.updateUser);
 
   return (
     <div className="flex min-h-svh text-slate-700 text-sm">
-      <Sidebar onUpdateUser={() => updateUser({ id: undefined, user_name: undefined })} />
+      <Sidebar
+        onUpdateUser={() => updateUser({ id: undefined, user_name: undefined })}
+      />
       <main className="flex-1 p-4 bg-slate-50">
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
           <Outlet />
         </ErrorBoundary>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
