9a184bc768b5085d30236a147bf7e832b8a42d70
iamelme <elme.delossantos@gmail.com>
2026-03-05 16:29:18 +0800

    chore: drop logs

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vitest/config" />
 
-import { resolve } from 'node:path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "node:path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -11,33 +11,33 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
-        external: ['better-sqlite3']
-      }
-    }
+        external: ["better-sqlite3"],
+      },
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
-        external: ['better-sqlite3']
-      }
-    }
+        external: ["better-sqlite3"],
+      },
+    },
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve(__dirname, './src/renderer/src'),
-      }
+        "@renderer": resolve(__dirname, "./src/renderer/src"),
+      },
     },
     esbuild: {
-      // pure: ['console.log'],
-      // drop: ["console", "debugger"],
+      pure: ["console.log"],
+      drop: ["console", "debugger"],
     },
     build: {
       rollupOptions: {
-        external: ['better-sqlite3']
-      }
+        external: ["better-sqlite3"],
+      },
     },
-    plugins: [react(), tailwindcss(), tsconfigPaths()]
-  }
-})
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
+  },
+});
