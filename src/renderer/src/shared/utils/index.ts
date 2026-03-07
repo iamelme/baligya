f8cac16116c47305f696aa823c443380a53d313a
iamelme <elme.delossantos@gmail.com>
2026-03-07 17:22:46 +0800

    refactor: format

--- src/renderer/src/shared/utils/index.ts
+++ src/renderer/src/shared/utils/index.ts
@@ -1,76 +1,85 @@
-export function humanize(str): string {
+export function humanize(str: string): string {
   return str
-    .replace(/^[\s_]+|[\s_]+$/g, '')
-    .replace(/[-_\s]+/g, ' ')
-    .replace(/([A-Z])/g, ' $1')
+    .replace(/^[\s_]+|[\s_]+$/g, "")
+    .replace(/[-_\s]+/g, " ")
+    .replace(/([A-Z])/g, " $1")
     .trim()
     .replace(/^[a-z]/, function (m) {
-      return m.toUpperCase()
-    })
+      return m.toUpperCase();
+    });
 }
 
 export const saleStatuses = [
   // { label: 'In-progress', value: 'in-progress' },
-  { label: 'Complete', value: 'complete' },
-  { label: 'Void', value: 'void' }
-]
+  { label: "Complete", value: "complete" },
+  { label: "Void", value: "void" },
+];
 
 export function addDays(date: Date, days: number): string {
   if (!date) {
-    return ''
+    return "";
   }
-  const newDate = new Date(date)
+  const newDate = new Date(date);
 
-  newDate.setDate(date.getDate() + days)
-  return newDate.toISOString()
+  newDate.setDate(date.getDate() + days);
+  return newDate.toISOString();
 }
 
 export function csvDownload<T>({
   header,
   data,
-  title
+  title,
 }: {
-  header: Array<string>
-  data: T
-  title: string
+  header: Array<string>;
+  data: T;
+  title: string;
 }): void {
   const content = Array.isArray(data)
-    ? data?.map((d) => Object.values(d).join(', ')).join('\n')
-    : ''
-  const csvString = `${header}\n` + content
-  const uri = encodeURI('data:text/csv;charset=utf-8,' + csvString)
+    ? data?.map((d) => Object.values(d).join(", ")).join("\n")
+    : "";
+  const csvString = `${header}\n` + content;
+  const uri = encodeURI("data:text/csv;charset=utf-8," + csvString);
 
-  const link = document.createElement('a')
-  link.setAttribute('href', uri)
-  link.setAttribute('download', `${title}.csv`)
-  document.body.appendChild(link)
+  const link = document.createElement("a");
+  link.setAttribute("href", uri);
+  link.setAttribute("download", `${title}.csv`);
+  document.body.appendChild(link);
 
-  link.click()
-  document.body.removeChild(link)
+  link.click();
+  document.body.removeChild(link);
 }
 
 export const numericFormatLimit =
   (maxValue: number) =>
-    (value): boolean => {
-      const { floatValue } = value
-      if (floatValue === undefined || floatValue <= maxValue) {
-        return true
-      }
-      return false
+  (value): boolean => {
+    const { floatValue } = value;
+    if (floatValue === undefined || floatValue <= maxValue) {
+      return true;
     }
+    return false;
+  };
 
-export function getCurrentQuarterDates(): { start: Date, end: Date, months: string[] } {
+export function getCurrentQuarterDates(): {
+  start: Date;
+  end: Date;
+  months: string[];
+} {
   const today = new Date();
   const currentMonth = today.getMonth();
   const currentYear = today.getFullYear();
-  const months: string[] = []
+  const months: string[] = [];
 
   const quarterStartMonth = Math.floor(currentMonth / 3) * 3;
 
   const startDate = new Date(currentYear, quarterStartMonth, 1);
 
   for (let i = 1; i <= 3; i++) {
-    months.push(new Date(currentYear, quarterStartMonth + i, 0).toLocaleString('default', { month: 'long' }))
+    months.push(
+      new Date(currentYear, quarterStartMonth + i, 0).toLocaleString(
+        "default",
+        { month: "long" },
+      ),
+    );
   }
 
   const endDate = new Date(currentYear, quarterStartMonth + 3, 0);
@@ -82,9 +91,13 @@ export function getCurrentQuarterDates(): { start: Date, end: Date, months: stri
   };
 }
 
-
-export function downloadblePDF({ res, invoiceNumber }: { res: ArrayBuffer, invoiceNumber: string }) {
-
+export function downloadblePDF({
+  res,
+  invoiceNumber,
+}: {
+  res: ArrayBuffer;
+  invoiceNumber: string;
+}) {
   const pdf = new Blob([res], { type: "application/pdf" });
 
   console.log(pdf);
