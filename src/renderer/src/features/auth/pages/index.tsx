b5729f2706ba3651ee9afc8910fb64a63912ac75
iamelme <elme.delossantos@gmail.com>
2026-03-10 01:04:30 +0800

    refactor: add logo

--- src/renderer/src/features/auth/pages/index.tsx
+++ src/renderer/src/features/auth/pages/index.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
+import myImage from "../../../../../../resources/icon.png"; // Path relative to the JS file
 
 export default function Auth(): ReactNode {
   return (
-    <div className="flex gap-x-5 p-5 min-h-[100svh] ">
-      <div className="w-[50%] p-[5%] rounded-lg bg-gradient-to-r from-violet-600 to-indigo-600 ">
+    <div className="flex gap-x-5 p-5 max-w-5xl min-h-[100svh] mx-auto ">
+      <div className="w-[50%] p-[5%] rounded-2xl bg-gradient-to-r from-violet-600 to-indigo-600 ">
         <h2 className="mb-4 text-2xl text-white">
           Stay on top of your sales and performance.
         </h2>
@@ -12,6 +13,7 @@ export default function Auth(): ReactNode {
           Easily monitor your revenue, track returns, and see your top-selling
           products.
         </p>
+        <img src={myImage} alt="baligya logo" />
       </div>
       <div className="w-[50%] p-5">
         <Outlet />
