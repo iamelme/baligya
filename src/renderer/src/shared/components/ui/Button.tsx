ae18c35167202c6f2625a777e496d26a34a5fc7d
iamelme <elme.delossantos@gmail.com>
2026-03-10 01:03:18 +0800

    refactor: add xs size option and format

--- src/renderer/src/shared/components/ui/Button.tsx
+++ src/renderer/src/shared/components/ui/Button.tsx
@@ -1,31 +1,35 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react'
-import { twMerge } from 'tailwind-merge'
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 const styles = {
   variants: {
-    default: 'bg-indigo-600 text-white',
-    danger: 'bg-red-600 text-white',
-    outline: 'border border-slate-400'
+    default: "bg-indigo-600 text-white",
+    danger: "bg-red-600 text-white",
+    outline: "border border-slate-400",
   },
   sizes: {
-    default: 'py-2 px-4',
-    icon: 'p-2',
-    sm: 'py-2 px-4 text-xs'
-  }
-}
+    default: "py-2 px-4",
+    icon: "p-2",
+    sm: "py-2 px-4 text-xs",
+    xs: "py-2 px-3 text-xs",
+  },
+};
 
-type Styles = typeof styles
+type Styles = typeof styles;
 
 export type ButtonProps = {
-  variant?: keyof Styles['variants']
-  size?: keyof Styles['sizes']
-  full?: boolean
-  children: ReactNode
-} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+  variant?: keyof Styles["variants"];
+  size?: keyof Styles["sizes"];
+  full?: boolean;
+  children: ReactNode;
+} & DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
 export default function Button({
-  variant = 'default',
-  size = 'default',
+  variant = "default",
+  size = "default",
   full = false,
   children,
   ...props
@@ -35,10 +39,10 @@ export default function Button({
       {...props}
       className={twMerge(
         `inline-flex  justify-center items-center gap-x-1 ${styles.variants[variant]} ${styles.sizes[size]} rounded-sm  cursor-pointer`,
-        `${props.className} ${full ? 'w-full' : ''} ${props.disabled ? 'opacity-75 cursor-default' : ''}`
+        `${props.className} ${full ? "w-full" : ""} ${props.disabled ? "opacity-75 cursor-default" : ""}`,
       )}
     >
       {children}
     </button>
-  )
+  );
 }
