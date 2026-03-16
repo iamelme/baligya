9c7037bf4b87ad2bacd82ca035815f48bd95300d
iamelme <elme.delossantos@gmail.com>
2026-03-16 23:42:23 +0800

    feature: add global mutation cache and toastify

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,18 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './assets/styles.css?assets'
+import "./assets/styles.css?assets";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-import Router from './app/router'
+import Router from "./app/router";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  }),
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router />
+      <ToastContainer />
     </QueryClientProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
