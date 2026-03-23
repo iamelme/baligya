492d17845cbb2425926d7aca08defb519a797241
iamelme <elme.delossantos@gmail.com>
2026-03-24 01:03:12 +0800

    refactor: format

--- src/renderer/src/shared/hooks/useTopItems.tsx
+++ src/renderer/src/shared/hooks/useTopItems.tsx
@@ -1,37 +1,41 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query'
-import { ProductType } from '../utils/types'
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { ProductType } from "../utils/types";
 
 type Props = {
-  pageSize?: number
-  startDate: Date | string
-  endDate: Date | string
-}
+  pageSize?: number;
+  startDate: Date | string;
+  endDate: Date | string;
+};
 
-type Res = Array<Pick<ProductType, 'id' | 'name'> & { net_quantity_sold: number }> | null
+type Res = Array<
+  Pick<ProductType, "id" | "name"> & { net_quantity_sold: number }
+> | null;
 
 export default function useTopItems({
   pageSize = 10,
   startDate,
-  endDate
+  endDate,
 }: Props): UseQueryResult<Res, Error> {
   return useQuery({
-    queryKey: ['top-items', startDate, endDate, pageSize],
+    queryKey: ["top-items", startDate, endDate, pageSize],
     queryFn: async () => {
       const { data, error } = await window.apiSale.getTopItems({
         pageSize,
         cursorId: 0,
         lastTotal: 0,
-        startDate: !isNaN(new Date(startDate).valueOf()) ? new Date(startDate).toISOString() : '',
-        endDate: !isNaN(new Date(endDate).valueOf()) ? new Date(endDate).toISOString() : ''
-      })
+        startDate: !isNaN(new Date(startDate).valueOf())
+          ? new Date(startDate).toISOString()
+          : "",
+        endDate: !isNaN(new Date(endDate).valueOf())
+          ? new Date(endDate).toISOString()
+          : "",
+      });
 
       if (error instanceof Error) {
-        throw new Error(error.message)
+        throw new Error(error.message);
       }
 
-      console.log('top data', data)
-
-      return data
-    }
-  })
+      return data;
+    },
+  });
 }
