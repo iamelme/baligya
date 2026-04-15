102e5c170b061859f78767e638ed91e0d72a58f3
iamelme <elme.delossantos@gmail.com>
2026-04-15 21:52:54 +0800

    refactor: redirect on success

--- src/renderer/src/features/salesOrders/hooks/useCreateSalesOrder.ts
+++ src/renderer/src/features/salesOrders/hooks/useCreateSalesOrder.ts
@@ -1,21 +1,37 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { CreateSalesOrderParams } from "src/main/interfaces/ISalesOrderRepository";
 
 export default function useCreateSalesOrder() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (data: CreateSalesOrderParams) => {
-      const { success, error } = await window.apiSalesOrder.create(data);
+      const {
+        success,
+        data: returnData,
+        error,
+      } = await window.apiSalesOrder.create(data);
 
       if (error instanceof Error) {
         throw new Error(error.message);
       }
 
+      if (success) {
+        return returnData;
+      }
+
       return success;
     },
-    onSuccess: () => {
-      navigate(-1);
+    onSuccess: (data) => {
+      if (data) {
+        navigate(`/so/${data.id}`);
+      }
+
+      queryClient.invalidateQueries({
+        queryKey: ["sales-order", `${data ? data.id : ""}`],
+      });
     },
   });
 }
--- src/renderer/src/features/salesOrders/hooks/useUpdateSalesOrder.ts
+++ src/renderer/src/features/salesOrders/hooks/useUpdateSalesOrder.ts
@@ -1,21 +1,46 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { UpdateSalesOrderParams } from "src/main/interfaces/ISalesOrderRepository";
 
 export default function useUpdateSalesOrder() {
   const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: async (data: UpdateSalesOrderParams) => {
-      const { success, error } = await window.apiSalesOrder.update(data);
+      const {
+        success,
+        data: returnData,
+        error,
+      } = await window.apiSalesOrder.update(data);
 
       if (error instanceof Error) {
         throw new Error(error.message);
       }
 
+      if (success) {
+        return returnData;
+      }
+
       return success;
     },
-    onSuccess: () => {
-      navigate(-1);
+    onSuccess: (data, variables) => {
+      console.log(variables.id);
+      queryClient.invalidateQueries({
+        queryKey: ["sales-order", `${variables?.id}`],
+      });
+
+      console.log({ data, variables });
+
+      if (data && data?.id) {
+        navigate(`/sales/${data.id}`);
+        return;
+      }
+
+      if (variables?.id) {
+        navigate(`/so/${variables.id}`);
+      }
     },
   });
 }
