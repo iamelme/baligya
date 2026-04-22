38b52c30540bd0c35f8905123f2a69c6b3cc1998
iamelme <elme.delossantos@gmail.com>
2026-04-22 22:58:23 +0800

    fix: renderer state loss when minimize/hide

--- src/main/index.ts
+++ src/main/index.ts
@@ -50,6 +50,7 @@ function createWindow(): void {
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
       sandbox: false,
+      backgroundThrottling: false,
     },
   });
 
